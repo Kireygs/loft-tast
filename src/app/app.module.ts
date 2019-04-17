35ad63241496622de8763bd293623681f0a0fb2e
kireygs <gabit145k@gmail.com>
2019-04-17 15:21:58 +0200

    Graphic almost done, service created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,26 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import {FlexLayoutModule} from '@angular/flex-layout';
+import {MatCardModule, MatToolbarModule} from '@angular/material';
+import { DetailViewComponent } from './detail-view/detail-view.component';
+import {AppService} from './app.service';
+import {HttpClientModule} from '@angular/common/http';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DetailViewComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    FlexLayoutModule,
+    MatToolbarModule,
+    MatCardModule
   ],
-  providers: [],
+  providers: [AppService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
